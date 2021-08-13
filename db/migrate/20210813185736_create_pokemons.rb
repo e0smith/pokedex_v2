c5b1ee9c81049408ca426f7abb77ecab91612a52
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-08-13 15:27:17 -0400

    pokemons migration testing

--- db/migrate/20210813185736_create_pokemons.rb
+++ db/migrate/20210813185736_create_pokemons.rb
@@ -1,7 +1,10 @@
 class CreatePokemons < ActiveRecord::Migration[6.1]
   def change
     create_table :pokemons do |t|
-
+      t.string :name
+      t.text :moves, moves: {name: 'name'}
+      t.text :types, types: {name: 'name'}
+      t.text :abilities, abilities: {name: 'name'}
       t.timestamps
     end
   end
